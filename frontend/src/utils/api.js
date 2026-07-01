af5a4a72b2c591738d5943e793e518db571a8061
gouking <gouking@gmail.com>
2026-07-01 23:02:35 +0800

    hardcode backend api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL:  'https://tablet-production-2ccc.up.railway.app/api',
 })
 
 api.interceptors.request.use(config => {
